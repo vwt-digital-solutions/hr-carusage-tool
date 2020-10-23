fb25f907629d2d47afc3ae39aaf2647b5e8ae754
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2020-10-23 14:42:48 +0200

    Added driver info to trip info. DAT-4501

--- src/app/models/trip.model.ts
+++ src/app/models/trip.model.ts
@@ -1,15 +1,25 @@
 import { Geometry } from './geometry.model';
 
 export interface Trip {
-    license: string;
-    started_at: string;
-    ended_at: string;
-    locations: TripLocation[];
+  license: string;
+  started_at: string;
+  ended_at: string;
+  driver_info: DriverInfo;
+  locations: TripLocation[];
 }
 
 export interface TripLocation {
-    geometry: Geometry;
-    token: string;
-    what: string;
-    when: string;
+  geometry: Geometry;
+  token: string;
+  what: string;
+  when: string;
+}
+
+export interface DriverInfo {
+  department: number;
+  function_name: string;
+  initial: string;
+  last_name: string;
+  prefix: string;
+  registration_number: number;
 }
